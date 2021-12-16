7a5ff91df22a53a37a519af587a5be40541a7c6e
lgrachov <78234184+lgrachov@users.noreply.github.com>
2021-12-16 15:31:07 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+namespace SpriteKind {
+    export const _Combo = SpriteKind.create()
+}
+let Mant = sprites.create(assets.image`_Моя Рука`, SpriteKind._Combo)
+if (true) {
+    music.playMelody("B A G A G F A C5 ", 120)
+}
